a9fbafccbbe21f8f4bfaf2031c063b1c792c01f3
plandevida <plandevida@planes.com>
2013-05-04 17:13:18 +0200

    Initial Commit

---
+++ Practica3-EDA/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Practica3-EDA
+//
+//  Created by Dani on 04/05/13.
+//  Copyright (c) 2013 EDA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
